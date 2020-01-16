932201a3f8a14f777448a4a08ea35bc158c2d6f9
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-01-16 14:32:47 +0800

    code splitting/ lazy loading components

---
+++ src/Components/CodeSplitting/LargeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const space = require('../../assets/img/space.jpeg');
+
+const LargeComponent = () => {
+    return (
+        <img style={{width: '300px', height: '300px'}} alt="banking at your doorstep" src={space}></img>
+    )
+}
+
+export default LargeComponent;
