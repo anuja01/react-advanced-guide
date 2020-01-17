8b93878e683670bcb8acb8dfdc6a7cbd169878ce
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-01-17 13:28:48 +0800

    dynamic context for theme

--- src/Modules/Main.js
+++ src/Modules/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CodeSplitting from '../Components/CodeSplitting'
 import App from '../Components/Context/WithoutContext';
 import AppWithContext from '../Components/Context/WithContext';
+import DynamicContext from '../Components/Context/DynamicContext';
 const Main = () => {
     return (
         <div>
@@ -9,7 +10,11 @@ const Main = () => {
             <br />
             <App />
             <br />
+            <div>--------- With Context ----------</div>
             <AppWithContext />
+            <br />
+            <div>--------- Dynamic Context (Changing themes)--------</div>
+            <DynamicContext />
         </div>
     )
 }
